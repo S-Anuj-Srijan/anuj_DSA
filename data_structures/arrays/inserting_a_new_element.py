58839f52d4bc2c401bb86e815c157c410b2982d1
S-Anuj-Srijan <anuj.srijan.s@gmail.com>
2025-02-18 14:52:00 +0530

    arrays cont

--- data_structures/arrays/inserting_a_new_element.py
+++ data_structures/arrays/inserting_a_new_element.py
@@ -1,18 +1,20 @@
 a = [1,2,3,5,6,7,8,9,0]
-b=[]
+b=[1,2,3,5,6,7,8,9,0]
 #to insert a valeu 4 in 3 index we first have to make space 
 a.append(a[-1])
 count=len(a)-2
-print(a)
 #this loop runs for n times in the worst case scenario thus the notation 0(n)
 for i in reversed(a[0:len(a)-2]):
-    print(i)
-    print(f"count{count}")
     a[count]=i
     count=count-1
     if count==3:
         a[3]=4
         break
+#u can also use .insert() method in python 
+b.insert(3,4)
+print(b)
+print(a)
+
 
 
     
